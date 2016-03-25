a80888a3897d5daf22af0a5c1319a51aa8c8071e
Barackbar <jd.sprunger@mac.com>
2016-03-24 19:25:08 -0500

    initial commit

--- ALJuniorRangers/app/src/main/java/com/au_team11/aljuniorrangers/ActionPointPicture.java
+++ ALJuniorRangers/app/src/main/java/com/au_team11/aljuniorrangers/ActionPointPicture.java
@@ -6,6 +6,8 @@ import android.provider.MediaStore;
 
 import com.esri.core.geometry.Point;
 
+import java.io.File;
+
 /**
  * Created by JDSS on 2/26/16.
  */
@@ -21,6 +23,17 @@ public class ActionPointPicture extends ActionPoint {
         super.action();
         //send camera intent
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //try creating the file
+        try {
+            File photo = File.createTempFile("file", ".jpg", activity.getApplicationContext().getFilesDir());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        
+
         if (takePictureIntent.resolveActivity(activity.getApplicationContext().getPackageManager()) != null) {
             activity.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
