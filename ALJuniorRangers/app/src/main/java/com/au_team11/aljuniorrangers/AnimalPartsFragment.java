f12959a29fa9b166443a14e717d36dd01607fe6d
Barackbar <jd.sprunger@mac.com>
2016-03-29 19:42:02 -0500

    loads data from json file

--- ALJuniorRangers/app/src/main/java/com/au_team11/aljuniorrangers/AnimalPartsFragment.java
+++ ALJuniorRangers/app/src/main/java/com/au_team11/aljuniorrangers/AnimalPartsFragment.java
@@ -2,8 +2,10 @@ package com.au_team11.aljuniorrangers;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -12,12 +14,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 /**
  * Created by JDSS on 3/28/16.
  */
 public class AnimalPartsFragment extends Fragment {
 
     Activity mCallback;
+    Context context;
 
     View view;
 
@@ -25,12 +35,17 @@ public class AnimalPartsFragment extends Fragment {
 
     Bitmap pic;
 
+    //hitboxes, defined in percentage from left and top
+    ArrayList<Rect> hitboxes;
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
 
         try {
             mCallback = (Activity) activity;
+            context = activity.getApplicationContext();
+
         } catch (ClassCastException e) {
             e.printStackTrace();
         }
@@ -54,28 +69,87 @@ public class AnimalPartsFragment extends Fragment {
         animalPartsView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                /*Toast toast = Toast.makeText(mCallback, "X: " + Float.toString(event.getX()) +
-                                                        " Y: " + Float.toString(event.getY()) +
-                                                        " RX: " + Float.toString(event.getRawX()) +
-                                                        " RY: " + Float.toString(event.getRawY()), Toast.LENGTH_SHORT);
-                */
-                if (animalPartsView.isInPic((int) event.getX(), (int) event.getY())) {
-                    Toast toast = Toast.makeText(mCallback, "is in pic", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-                else {
-                    Toast toast = Toast.makeText(mCallback, "is NOT in pic", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
 
+                Toast toast = Toast.makeText(
+                                        mCallback.getApplicationContext(),
+                                        "in hitbox " + Integer.toString(animalPartsView.isInBox((int) event.getX(), (int) event.getY(), hitboxes)),
+                                        Toast.LENGTH_SHORT);
+                toast.show();
                 return true;
             }
         });
 
-        pic = BitmapFactory.decodeResource(mCallback.getApplicationContext().getResources(), R.drawable.deerimage);
+        String jsonData = loadJSONFromAsset("animalparts_data_demo_c1.json");
+
+        String picName = getPicName(jsonData);
+
+        int picID = context.getResources().getIdentifier(picName, "drawable", context.getPackageName());
+
+        pic = BitmapFactory.decodeResource(mCallback.getApplicationContext().getResources(), picID);
 
         animalPartsView.setPic(pic);
 
+        hitboxes = buildHitboxes(jsonData);
+
+        /*
+        hitboxes = new ArrayList<Rect>();
+        hitboxes.add(new Rect(0,0,25,25));
+        hitboxes.add(new Rect(25,25,50,50));
+        hitboxes.add(new Rect(50,50,75,75));
+        hitboxes.add(new Rect(75,75,100,100));
+        */
+
         return view;
+    }
+
+    //credit goes to GrlsHu on StackOverflow
+    //returns a json string from the asset file
+    public String loadJSONFromAsset(String fileName) {
+        String json = null;
+        try {
+            InputStream is = getActivity().getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    public String getPicName(String jsonData) {
+        try {
+            return new JSONObject(jsonData).getString("image");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList<Rect> buildHitboxes(String jsonData) {
+        try {
+            ArrayList<Rect> newHitboxes = new ArrayList<Rect>();
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray jsonArray = jsonObject.getJSONArray("hitboxes");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject arrayObject = jsonArray.getJSONObject(i);
+                //add a new rect to the list
+                newHitboxes.add(
+                        new Rect(
+                                arrayObject.getInt("left"),
+                                arrayObject.getInt("top"),
+                                arrayObject.getInt("right"),
+                                arrayObject.getInt("bottom")));
+            }
+            return newHitboxes;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- ALJuniorRangers/app/src/main/java/com/au_team11/aljuniorrangers/AnimalPartsView.java
+++ ALJuniorRangers/app/src/main/java/com/au_team11/aljuniorrangers/AnimalPartsView.java
@@ -8,10 +8,15 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+
 /**
  * Created by JDSS on 3/28/16.
  */
 public class AnimalPartsView extends View {
+
+    //the amount of canvas size difference to cause a re-scaling of the picture
+    public static final double EPSILON = 0.00001;
 
     //the picture to show on screen
     Bitmap pic;
@@ -20,7 +25,11 @@ public class AnimalPartsView extends View {
 
     //rectangle that holds the bitmap pic, note this is relative to the canvas provided in onDraw
     Rect destRect;
-    Rect srcRect;
+    //the width to height ratio of the canvas
+    double canvasWHR;
+    //the width to height ratio of the picture
+    double picWHR;
+
 
     public AnimalPartsView(Context context) {
         super(context);
@@ -42,60 +51,60 @@ public class AnimalPartsView extends View {
         this.pic = pic;
     }
 
-    public Boolean isInPic(int x, int y) {
+    public int isInBox(int x, int y, ArrayList<Rect> hitboxes) {
 
-        //TODO: make this method take in an arraylist of rects maybe, and return back the index of
-        //the rect it is in or -1 for inside none
-
-        if (x > destRect.left && x < destRect.right/2 && y > destRect.top && y < destRect.bottom/2) {
-            return true;
+        //search through the hitboxes
+        for (int i = 0; i < hitboxes.size(); i++) {
+            //if x,y is within the current hitbox area (hitboxes defined in percentage)
+            //x and y are integer pixels, divide by width or height to get decimal percentage from left or top
+            //multiply by 100 to get integer percentage, which the hitboxes are defined as
+            if (hitboxes.get(i).contains((int) (((double) x / destRect.width()) * 100),
+                                         (int) (((double) y / destRect.height()) * 100))) {
+                return i;
+            }
         }
 
-        return false;
+        //if it can't find a hitbox that contains x,y, return -1
+        return -1;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if (!picScaled)
+        //if the difference between the previous width/height is greater than epsilon, re-scale destRect
+        if (Math.abs(canvasWHR - ((double) canvas.getWidth() / (double) canvas.getHeight())) > EPSILON)
             scalePic(canvas);
 
         canvas.drawBitmap(pic, null, destRect, null);
-
     }
 
     private void scalePic(Canvas canvas) {
 
         picScaled = true;
 
-        //create rect for picture
+        //create rect to hold picture
         destRect = new Rect();
 
-        //srcRect = new Rect(0, 0, pic.getWidth(), pic.getHeight());
+        //find the width to height ratios of the picture and provided canvas
+        picWHR = (double) pic.getWidth() / pic.getWidth();
+        canvasWHR = (double) canvas.getWidth() / canvas.getHeight();
 
-
-        //find width/height of picture
-        //find width/height of canvas
-        //if canvas w/h > picture w/h
-            //destRect L & R = 0 & canvas.width
-            //destRect height = destRect width / pic w/h
-            //destRect top = (canvas.height - destRect height) / 2
-            //destRect bottom  = canvas.height - ((canvas.height - destRect height) / 2)
-
-        double picWHR = (double) pic.getWidth() / pic.getWidth();
-        double canvasWHR = (double) canvas.getWidth() / canvas.getHeight();
-
+        //if the canvas is skinnier than or equal to the picture
         if (canvasWHR <= picWHR) {
             destRect.left = 0;
             destRect.right = canvas.getWidth();
-            int dRHeight = destRect.width() / (int) picWHR;
+            int dRHeight = (int) (destRect.width() / picWHR);
             destRect.top = (canvas.getHeight() - dRHeight) / 2;
             destRect.bottom = canvas.getHeight() - ((canvas.getHeight() - dRHeight) / 2);
         }
+        //else the canvas is wider than the picture
         else {
-
+            destRect.top = 0;
+            destRect.bottom = canvas.getHeight();
+            int dRWidth = (int) (destRect.height() * picWHR);
+            destRect.left = (canvas.getWidth() - dRWidth) / 2;
+            destRect.right = canvas.getWidth() - ((canvas.getWidth() - dRWidth) / 2);
         }
-
     }
 }
